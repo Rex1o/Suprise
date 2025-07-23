d58809a2c3d1e1c11c8c6c07f86d31f9045ec983
Rex1o <70145948+Rex1o@users.noreply.github.com>
2025-07-23 14:24:32 -0400

    Version final

--- main.py
+++ main.py
@@ -37,69 +37,82 @@ def snr_interval(m):
     max_snr = min_snr + 20
     return (min_snr, max_snr)
 
-def ask_modulate(symbols, M, fc, fs, Tb, snr=0):
+
+AMP_PORTEUSE = 1 # amp max de la porteuse
+POINTS_PER_PERIOD = 200
+
+def ask_modulate(symbols, M, fc, snr=0):
     """Modulation ASK (Amplitude Shift Keying)"""
+    period = 1 / fc
+    gap = AMP_PORTEUSE / M
+    amp_lookup_table = np.linspace(gap, AMP_PORTEUSE, num=M, endpoint=True)
+    amp_values = amp_lookup_table[symbols]
+    sinValues = np.zeros(len(symbols) * POINTS_PER_PERIOD)
+    timeValues = np.linspace(0, len(symbols) * period, num=POINTS_PER_PERIOD * len(symbols), endpoint=True)
+    for i in range(len(symbols)):
+        index1 = i * POINTS_PER_PERIOD
+        index2 = index1 + POINTS_PER_PERIOD - 1
+        sinValues[index1:index2] = amp_values[i] * np.sin(2 * math.pi * fc * timeValues[index1:index2])
 
-    t = np.arange(0, Tb*len(symbols), 1/fs)
-    signal = np.zeros_like(t)
-    amp_levels = np.linspace(0, 1, M)
-    for i, sym in enumerate(symbols):
-        start = int(i*Tb*fs)
-        end = int((i+1)*Tb*fs)
-        A = amp_levels[sym]
-        signal[start:end] = A * np.cos(2*np.pi*fc*t[start:end])
-    # Ajout de bruit si SNR > 0
     if snr > 0:
-        noise = np.random.normal(0, 10**(-snr/20), size=signal.shape)
-        signal += noise
-    return t, signal
+        noise = np.random.normal(0, 10**(-snr/20), size=sinValues.shape)
+        sinValues += noise
 
-def fsk_modulate(symbols, fc, fs, M, Tb, spacing=1000, snr=0):
+    vline_time_index = np.arange(0, len(sinValues), POINTS_PER_PERIOD)
+    vlines = timeValues[vline_time_index]
+
+    return timeValues, sinValues, vlines
+
+def fsk_modulate(symbols, M, fc, snr=0, bandwidth=1000):
     """Modulation FSK (Frequency Shift Keying)"""
+    period = 1 / fc
+    freq_lookup_table = np.linspace(fc, fc + bandwidth, num=M, endpoint=True)
+    freq_values = freq_lookup_table[symbols]
+    sinValues = np.zeros(len(symbols) * POINTS_PER_PERIOD)
+    timeValues = np.linspace(0, len(symbols) * period, num=POINTS_PER_PERIOD * len(symbols), endpoint=True)
+    for i in range(len(symbols)):
+        index1 = i * POINTS_PER_PERIOD
+        index2 = index1 + POINTS_PER_PERIOD - 1
+        sinValues[index1:index2] = AMP_PORTEUSE * np.sin(2 * math.pi * freq_values[i] * timeValues[index1:index2])
 
-    t = np.arange(0, Tb*len(symbols), 1/fs)
-    signal = np.zeros_like(t)
-
-    freq_base = fc - (M-1) * spacing / 2  # Fréquence de base
-    for i, sym in enumerate(symbols):
-        start = int(i*Tb*fs)
-        end = int((i+1)*Tb*fs)
-        freq = freq_base + sym * spacing
-        signal[start:end] = np.cos(2 * np.pi * freq * t[start:end])
-    # Ajout de bruit si SNR > 0
     if snr > 0:
-        noise = np.random.normal(0, 10**(-snr/20), size=signal.shape)
-        signal += noise
-    return t, signal
+        noise = np.random.normal(0, 10**(-snr/20), size=sinValues.shape)
+        sinValues += noise
+
+    vline_time_index = np.arange(0, len(sinValues), POINTS_PER_PERIOD)
+    vlines = timeValues[vline_time_index]
+
+    return timeValues, sinValues, vlines
 
 # Modulation PSK (Phase Shift Keying)
-def psk_modulate(symbols, fc, fs, M, Tb, snr=0):
+def psk_modulate(symbols, M, fc, snr=0):
     """Modulation PSK (Phase Shift Keying)"""
-
-    t = np.arange(0, Tb*len(symbols), 1/fs)
-    signal = np.zeros_like(t)
-    angles = np.linspace(0, 2 * np.pi, M, endpoint=False)  # Angles pour chaque symbole
-    for i, sym in enumerate(symbols):
-        start = int(i*Tb*fs)
-        end = int((i+1)*Tb*fs)
-        phi = angles[sym]
-        signal[start:end] = np.cos(2 * np.pi * fc * t[start:end] + phi)
-    # Ajout de bruit si SNR > 0
+    period = 1 / fc
+    phase_end =  np.pi * 2 - np.pi * 2 / M
+    phase_lookup_table = np.linspace(0, phase_end , num=M, endpoint=True)
+    phase_values = phase_lookup_table[symbols]
+    sinValues = np.zeros(len(symbols) * POINTS_PER_PERIOD)
+    timeValues = np.linspace(0, len(symbols) * period, num=POINTS_PER_PERIOD * len(symbols), endpoint=True)
+    for i in range(len(symbols)):
+        index1 = i * POINTS_PER_PERIOD
+        index2 = index1 + POINTS_PER_PERIOD - 1
+        sinValues[index1:index2] = AMP_PORTEUSE * np.sin(2 * math.pi * fc * timeValues[index1:index2] + phase_values [i])
     if snr > 0:
-        noise = np.random.normal(0, 10**(-snr/20), size=signal.shape)
-        signal += noise
-    return t, signal
+        noise = np.random.normal(0, 10 ** (-snr / 20), size=sinValues.shape)
+        sinValues += noise
 
+    vline_time_index = np.arange(0, len(sinValues), POINTS_PER_PERIOD)
+    vlines = timeValues[vline_time_index]
+
+    return timeValues, sinValues, vlines
 
 def bit_to_symbol(bin_seq, n):
     """Convertit une séquence binaire en symboles avec n bits par symbole."""
-
     symbols = []
     for i in range(0, len(bin_seq), n):
         symbol = bin_seq[i:i+n]
         symbols.append(int(symbol, 2))
     return symbols
-
 
 def main():
     """Fonction principale pour l'interface graphique de modulation de signaux."""
@@ -107,7 +120,6 @@ def main():
     # Interface graphique avec matplotlib
     fig = plt.figure(figsize=(10, 5))
     plot_ax = fig.add_axes([0.05, 0.15, 0.65, 0.75])
-    line, = plot_ax.plot([], [])
     plot_ax.set_title("Signal modulé")
     plot_ax.set_xlabel("Temps (s)")
     plot_ax.set_ylabel("Amplitude")
@@ -116,7 +128,7 @@ def main():
     # Textbox pour la séquence binaire
     fig.text(0.875, 0.85, 'Séquence binaire', ha='center', va='bottom', fontsize=10)
     ax_bin_seq = plt.axes([0.80, 0.8, 0.15, 0.05])
-    text_bin_seq = TextBox(ax_bin_seq, "", initial='10110010')
+    text_bin_seq = TextBox(ax_bin_seq, "", initial='00100111')
 
     # Radio buttons pour la modulation
     fig.text(0.875, 0.75, 'Modulation', ha='center', va='bottom', fontsize=10)
@@ -131,12 +143,15 @@ def main():
     # Textbox de la bande passante
     fig.text(0.875, 0.50, 'Bande passante (Hz)', ha='center', va='bottom', fontsize=10)
     ax_bandwidth = plt.axes([0.80, 0.45, 0.15, 0.05])
-    text_bandwidth = TextBox(ax_bandwidth, '', initial='2000')
+    text_bandwidth = TextBox(ax_bandwidth, '', initial='20000')
 
     # Textbox pour le SNR
     fig.text(0.875, 0.40, 'SNR (dB)', ha='center', va='bottom', fontsize=10)
     ax_snr_slider = plt.axes([0.80, 0.35, 0.15, 0.05])
     snr_slider = Slider(ax_snr_slider, '', snr_interval(4)[0], snr_interval(4)[1], valinit=0, valstep=0.1)
+
+    # Message d'erreur:
+    error_text = fig.text(0.875, 0.15, '', ha='center', va='top', fontsize=10, color='red', wrap=True)
 
     # Calcul de la capacité de Shannon-Hartley
     def shannon_capacity(bandwidth, snr):
@@ -146,8 +161,8 @@ def main():
         return bandwidth * np.log2(1 + 10**(snr / 10))
 
     # Afficher la capacité de Shannon-Hartley
-    shannon_text = fig.text(0.875, 0.30, 'Capacité de Shannon-Hartley: - bits/s', ha='center', va='bottom', fontsize=10)
-    
+    shannon_text = fig.text(0.875, 0.35, 'Capacité de Shannon-Hartley: - bits/s', ha='center', va='top', fontsize=10, wrap=True)
+
     def update_snr_on_m_change(text):
         """Met à jour le slider SNR en fonction de la valeur de M."""
         nonlocal snr_slider  # Permet de modifier la variable snr_slider dans la portée externe
@@ -193,54 +208,53 @@ def main():
         SNR = float(snr_slider.val)                     # Rapport signal sur bruit en dB
 
         bandwidth = float(text_bandwidth.text)          # Bande passante en Hz
-        Tb  = 0.01                                      # Durée d'un bit en secondes
-        fs = 100000                                     # Fréquence d'échantillonnage (doit être >> fc)
-        fc = bandwidth/2                                # Fréquence porteuse
 
         # Validation de la séquence binaire
         if not all(bit in '01' for bit in bin_seq):
-            print("La séquence binaire n'est pas valide. Elle doit contenir uniquement des 0 et des 1.")
+            error_text.set_text("La séquence binaire n'est pas valide. Elle doit contenir uniquement des 0 et des 1.")
             return
         
         # Validation de la modulation
         valid_modulations = ["ASK", "FSK", "PSK"]
         if modulation not in valid_modulations:
-            print(f"La modulation '{modulation}' n'est pas supportée. Modulations supportées: {valid_modulations}")
+            error_text.set_text(f"La modulation '{modulation}' n'est pas supportée. Modulations supportées: {valid_modulations}")
             return
         
         # Validation du M
         is_power, m_sugg = is_power_of_two(m)
         if not is_power:
-            print(f"Le paramètre M n'est pas une puissance de 2. M suggéré: {m_sugg}")
+            error_text.set_text(f"Le paramètre M n'est pas une puissance de 2. M suggéré: {m_sugg}")
             m = m_sugg
+            return
 
         # Calcul du nombre de bits par symbole
         n = math.log2(m)
 
         # Validation de la longueur de la séquence binaire
         if len(bin_seq) % n != 0:
-            print(f"La longueur de la séquence binaire ({len(bin_seq)}) n'est pas un multiple de {n}.")
+            error_text.set_text(f"La longueur de la séquence binaire ({len(bin_seq)}) n'est pas un multiple de {n}.")
             return
         
         # Validation du SNR
         if not is_valid_snr(SNR, m):
-            print(f"Le SNR n'est pas suffisant pour la modulation M-aire avec M={m}. Il doit être entre {snr_interval(m)[0]} et {snr_interval(m)[1]}.")
+            print(f"La longueur de la séquence binaire ({len(bin_seq)}) n'est pas un multiple de {n}.")
             return
 
         # Afficher le signal
         if modulation == "ASK":
-            t, signal = ask_modulate(symbols=bit_to_symbol(bin_seq, int(math.log2(m))), M=m, fc=1000, fs=100000, Tb=0.01, snr=SNR)
+            t, signal, vlines = ask_modulate(symbols=bit_to_symbol(bin_seq, int(math.log2(m))), M=m, fc=1000, snr=SNR)
         elif modulation == "FSK":
-            t, signal = fsk_modulate(symbols=bit_to_symbol(bin_seq, int(math.log2(m))), fc=1000, fs=100000, M=m, Tb=0.01, snr=SNR)
+            t, signal, vlines = fsk_modulate(symbols=bit_to_symbol(bin_seq, int(math.log2(m))), M=m, fc=1000, snr=SNR, bandwidth=bandwidth)
         elif modulation == "PSK":
-            t, signal = psk_modulate(symbols=bit_to_symbol(bin_seq, int(math.log2(m))), fc=1000, fs=100000, M=m, Tb=0.01, snr=SNR)
+            t, signal, vlines = psk_modulate(symbols=bit_to_symbol(bin_seq, int(math.log2(m))), M=m, fc=1000, snr=SNR)
         else:
             print(f"Modulation '{modulation}' non supportée.")
             return
-        
+
         # Affichage du signal avec matplotlib
         plot_ax.clear()
         plot_ax.plot(t, signal, label=f"Signal {modulation} (M={m})")
+        plot_ax.vlines(vlines, ymin= -AMP_PORTEUSE, ymax= AMP_PORTEUSE, color='r', linestyle='dashed')
         plot_ax.set_title(f"Signal {modulation} pour la séquence binaire")
         plot_ax.set_xlabel("Temps (s)")
         plot_ax.set_ylabel("Amplitude")
@@ -252,8 +266,9 @@ def main():
         shannon_text.set_text(f'Capacité de Shannon-Hartley: {capacity:.2f} bits/s')
         
         fig.canvas.draw_idle()  # Redessiner le graphique
+        error_text.set_text("")
 
-    # Bouton 
+    # Bouton
     button = Button(plt.axes([0.82, 0.2, 0.1, 0.05]), 'Exécuter')
     button.on_clicked(on_submit)
     
@@ -273,10 +288,6 @@ def main():
 
     # Afficher le graphique    
     plt.show()
-    
-
-
-
 
 if __name__ == "__main__":
     main()
